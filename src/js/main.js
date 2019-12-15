b1666337392a036b6c835542e57189b4fb5ea1ce
YalovikEA <yalovikea@gmail.com>
2019-12-15 19:49:19 +0300

    added old price and arrow

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,27 @@
 $(document).ready(function() {
   svg4everybody({});
 
-  $('.slider').owlCarousel({
-    // stagePadding: 305,
-    animateOut: 'fadeOut',
-    smartSpeed: 1000,
-    center: true,
-    loop: true,
-    margin: 45,
-    nav: true,
-    dots: false,
-    responsive:{
-        0:{
-            items: 1
-        },
-        600:{
-            items: 2
-        },
-        1000:{
-            items: 4
-        }
-    }
-  });
+  $('.slider').each(function() {
+    $(this).owlCarousel({
+      // stagePadding: 305,
+      animateOut: 'fadeOut',
+      smartSpeed: 1000,
+      center: true,
+      loop: true,
+      margin: 45,
+      nav: true,
+      dots: false,
+      responsive:{
+          0:{
+              items: 1
+          },
+          600:{
+              items: 2
+          },
+          1000:{
+              items: 4
+          }
+      }
+    });
+  })
 });
