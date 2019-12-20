8f4a64ab312b732adc28c91b96e6d2ef062ad6bb
YalovikEA <yalovikea@gmail.com>
2019-12-20 22:22:15 +0300

    added styles for mobile

--- src/js/main.js
+++ src/js/main.js
@@ -25,12 +25,47 @@ $(document).ready(function() {
     });
   })
 
+  var contactsToggle = $('.contacts__main')
+  var menuToggle = $('.menu-toggle__btn');
+  var mobileMenu = $('.mobile-menu');
+  var contactsWrapper = $('.mobile-menu__contacts-wrapper');
+  var menuWrapper = $('.mobile-menu__main-wrapper');
   var catalog = $('.catalog__toggler');
   var catalogList = $('.catalog__list');
   var catalogItem = $('.catalog__item');
   var subcatalog = $('.subcatalog');
   var subcatalogBlock = $('.subcatalog__block');
   var subcatalogList = $('.subcatalog__list');
+
+  contactsToggle.on('click', function() {
+    $('.mobile-menu .contacts__main').toggleClass('active');
+    if(!menuToggle.hasClass('active')) {
+      mobileMenu.toggleClass('active');
+      menuWrapper.fadeToggle(0);
+      contactsWrapper.fadeToggle(0);
+    } else {
+      menuWrapper.fadeToggle(0);
+      contactsWrapper.fadeToggle(0);
+    }
+  })
+
+  menuToggle.on('click', function() {
+    if(!mobileMenu.hasClass('active') && !$(this).hasClass('active')) {
+      $(this).addClass('active');
+      mobileMenu.addClass('active');
+      contactsWrapper.fadeOut(0);
+      $('.mobile-menu .contacts__main').removeClass('active');
+      menuWrapper.fadeIn(0);
+    } else if (mobileMenu.hasClass('active') && !$(this).hasClass('active')){
+      $(this).addClass('active');
+      contactsWrapper.fadeOut(0);
+      $('.mobile-menu .contacts__main').removeClass('active');
+      menuWrapper.fadeIn(0);
+    } else {
+      $(this).removeClass('active');
+      mobileMenu.removeClass('active');
+    }
+  })
 
   catalog.on('click', function() {
     $(this).toggleClass('active');
